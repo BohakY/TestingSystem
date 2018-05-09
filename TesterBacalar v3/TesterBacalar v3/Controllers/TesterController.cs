a229a07e3a60b620298348d17ad596ce31c31fd1
Yaroslav Bohak <yaroslavbohak@gmail.com>
2018-05-09 19:47:26 +0300

    update 09.05

--- TesterBacalar v3/TesterBacalar v3/Controllers/TesterController.cs
+++ TesterBacalar v3/TesterBacalar v3/Controllers/TesterController.cs
@@ -8,10 +8,14 @@ namespace TesterBacalar_v3.Controllers
 {
     public class TesterController : Controller
     {
+
+        TesterBacalarWorkBDEntities db = new TesterBacalarWorkBDEntities();
         // GET: Tester
         public ViewResult Tester()
         {
-            @ViewBag.Who = "Тестер панель";
+            var table = db.Tests.ToList();
+            @ViewBag.Tests = table;
+
             return View();
         }
 
@@ -24,6 +28,12 @@ namespace TesterBacalar_v3.Controllers
         public ViewResult bbb()
         {
             @ViewBag.Who = "bbb";
+            return View();
+        }
+
+        public ViewResult Question()
+        {
+            @ViewBag.Who = "tsdfgghfgjghjhjk";
             return View();
         }
     }
