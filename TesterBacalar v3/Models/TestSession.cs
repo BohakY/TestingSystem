a3b1fa405a7e8faf8e47c4779db6cee9fcc60ae0
Yaroslav Bohak <yaroslavbohak@gmail.com>
2018-05-22 18:32:05 +0300

    xfdvg

---
+++ TesterBacalar v3/Models/TestSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TesterBacalar_v3;
+
+namespace TesterBacalar_v3.Models
+{
+    public class TestSession
+    {
+        public Guid Id { get; set; }
+
+        public int CurrentQuestion { get; set; }
+
+        public int Result { get; set; }
+
+        public Tests CurrentTest { get; set; }
+    }
+}
